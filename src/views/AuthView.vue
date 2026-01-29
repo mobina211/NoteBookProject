e9903a3a2fcefa5981abce6cf66a2e7c4e43b934
mobina211 <kazemzadehmobina0@gmail.com>
2026-01-29 12:40:51 +0330

    captcha added

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -22,6 +22,33 @@
   </span>
 </div>
 
+<!--Image CAPTCHA (only login) -->
+<div v-if="mode === 'Login'" class="mt-1">
+  <div class="flex items-center gap-3">
+    <canvas
+      ref="canvasRef"
+      width="120"
+      height="40"
+      class="border rounded-lg bg-white"
+    ></canvas>
+
+    <button
+      class="text-sm text-purple-600 hover:underline"
+      @click="regenerateCaptcha"
+      type="button"
+    >
+      🔄
+    </button>
+  </div>
+
+  <input
+    v-model="captchaInput"
+    placeholder="کد داخل تصویر"
+    class="input mt-2"
+  />
+</div>
+
+
 
 
 <button class="btn" @click="submit">{{ mode }}</button>
@@ -43,6 +70,60 @@
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '@/store/auth'
+ // شروع کپچا
+import { onMounted} from 'vue'
+
+const captchaText = ref('')
+const captchaInput = ref('')
+const canvasRef = ref(null)
+
+const generateCaptchaText = () => {
+  const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789'
+  captchaText.value = Array.from({ length: 5 }, () =>
+    chars[Math.floor(Math.random() * chars.length)]
+  ).join('')
+}
+
+const drawCaptcha = () => {
+  const canvas = canvasRef.value
+  const ctx = canvas.getContext('2d')
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  // background
+  ctx.fillStyle = '#f3e8ff'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  // noise lines
+  for (let i = 0; i < 4; i++) {
+    ctx.strokeStyle = `rgba(120, 60, 200, 0.4)`
+    ctx.beginPath()
+    ctx.moveTo(Math.random() * 120, Math.random() * 40)
+    ctx.lineTo(Math.random() * 120, Math.random() * 40)
+    ctx.stroke()
+  }
+
+  // text
+  ctx.font = '22px sans-serif'
+  ctx.textBaseline = 'middle'
+
+  captchaText.value.split('').forEach((char, i) => {
+    ctx.save()
+     ctx.fillStyle = `hsl(${Math.random() * 360}, 70%, 50%)`
+    ctx.translate(20 + i * 18, 20)
+    ctx.rotate((Math.random() - 0.5) * 0.4)
+    ctx.fillText(char, 0, 0)
+    ctx.restore()
+  })
+}
+
+const regenerateCaptcha = () => {
+  generateCaptchaText()
+  drawCaptcha()
+  captchaInput.value = ''
+}
+
+onMounted(regenerateCaptcha)
+ // پایان کپچا
 
 
 const router = useRouter()
@@ -61,9 +142,25 @@ const toggle = () => mode.value = mode.value === 'Login' ? 'Register' : 'Login'
 
 
 const submit = () => {
-auth.login({ username: username.value })
-router.push('/notes')
+  if (mode.value === 'Login') {
+    if (
+      captchaInput.value.toUpperCase() !== captchaText.value
+    ) {
+      alert('❌ کپچا اشتباه است')
+      regenerateCaptcha()
+      return
+    }
+  }
+
+  auth.login({
+    username: username.value,
+    email: email.value,
+    password: password.value
+  })
+
+  router.push('/notes')
 }
+
 </script>
 
 <style scoped>
